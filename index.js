9089cb32ddcd0f2437a9bb8f7d24bdab4dcfd546
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-20 22:04:01 +0600

    added query

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello From Node Server!");
@@ -23,8 +24,18 @@ const users = [
   { id: 6, name: "sabila", email: "sabila@gmail.com", phone: "0178888888" },
   { id: 7, name: "sohana", email: "sohana@gmail.com", phone: "0178888888" },
 ];
+
+// filter by search query parameter
 app.get("/users", (req, res) => {
-  res.send(users);
+  if (req.query.name) {
+    const search = req.query.name.toLowerCase();
+    const matched = users.filter((user) =>
+      user.name.toLowerCase().includes(search)
+    );
+    res.send(matched);
+  } else {
+    res.send(users);
+  }
 });
 
 app.get("/user/:id", (req, res) => {
